ef47d4b8a45d30753a73109f0dd74bb715f564c4
Agnes Nuguid <agnes.nuguid@uap.asia>
2020-02-29 16:57:13 +0800

    Closes #1: Application Skeleton - Pull and display data from NBA

--- src/App.js
+++ src/App.js
@@ -2,23 +2,62 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Games extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          error: null,
+          isLoaded: false,
+          games: []
+      };
+  }
+
+  componentDidMount() {
+      fetch('http://data.nba.com/data/5s/json/cms/noseason/scoreboard/20200227/games.json')
+          .then (res => res.json())
+          .then(
+              (result) => {
+                  console.log(result.sports_content.games.game);
+                  this.setState({
+                      isLoaded: true,
+                      games: result.sports_content.games.game
+                  });
+                  
+              },
+              (error) => {
+                  this.setState({
+                      isLoaded: true,
+                      error
+                  });
+              }
+          )
+  }
+
+  render() {
+
+      const { error, isLoaded, games } = this.state;
+      if (error) {
+          return <div> Error: {error.message}</div>;
+      } else if (!isLoaded) {
+          return <div>Loading...</div>;
+      } else {
+          const g = games.map((game, index) => {
+              
+              return (
+                  <div key={game.id}>{game.home.city} {game.home.nickname} - {game.home.score} | {game.visitor.city} {game.visitor.nickname} - {game.visitor.score}</div>
+              );
+          });
+          return g;
+      }
+      
+  }
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Games />
     </div>
   );
 }
